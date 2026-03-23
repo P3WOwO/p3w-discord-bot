b1b5e6713527285e4c0332c97b8fcc671b820d6c
P3WOwO <p3wowo@gmail.com>
2026-03-24 01:42:46 +0300

    0.13

--- index.js
+++ index.js
@@ -191,6 +191,15 @@ async function registerCommands() {
 }
 
 client.once('clientReady', async () => {
+  client.user.setPresence({
+  activities: [
+    {
+      name: 'Пение птиц',
+      type: 2, // 0=играет, 1=стримит, 2=слушает, 3=смотрит, 5=соревнуется
+    },
+  ],
+  status: 'dnd', // online / idle / dnd / invisible
+});
   console.log(`✅ Бот онлайн: ${client.user.tag}`);
 
   loadData();
